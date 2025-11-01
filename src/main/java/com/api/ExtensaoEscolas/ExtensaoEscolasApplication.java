59ef7bec98485d04cffbae70ea04a862d777692a
Lealca10 <leandro.alcantara14@gmail.com>
2025-11-01 16:00:35 -0300

    Projeto de extensão

---
+++ src/main/java/com/api/ExtensaoEscolas/ExtensaoEscolasApplication.java
@@ -0,0 +1,13 @@
+package com.api.ExtensaoEscolas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtensaoEscolasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtensaoEscolasApplication.class, args);
+	}
+
+}
